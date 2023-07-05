b23ee53175bbe085295cc9cb58e8b2d1d968269c
Gabriel Meza <43562881+gabrie504@users.noreply.github.com>
2023-07-04 23:59:34 -0600

    mas modificaciones de la pagina principal

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -9,9 +9,9 @@
             align-items: center;
         }
 
-        .logo-tvc {
-            width: 100px;
-        }
+         .logo-tvc {
+            width: 150px;
+         }
 
         .item {
             text-align: center;
@@ -43,14 +43,21 @@
 </head>
 
 <body>
-    <nav class="container-nav">
-        <a href="{{secure_url(route('home'))}}">
-            <img src="{{ secure_asset('tvc.png') }}" class="logo-tvc" alt="logo tvc">
-        </a>
+    {{--***************************************************  --}}
+    <nav class="navbar bg-body-tertiary ">
+        <div class="container-fluid justify-content-start">
+         
 
-        <h1 class="item">IT</h1>
+          <button class="navbar-toggler " type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
 
-        <div class="item">
+          
+          {{-- +++++++++++++++++++++lo mio++++++++++++++++++++++++++++++++ --}}
+
+
+
+        <div class="item ms-auto">
             <ul class="nav justify-content-end">
                 <li class="nav-item">
                     <a class="nav-link active" aria-current="page" href="{{secure_url(route('home'))}}">Home</a>
@@ -72,17 +79,60 @@
                     </div>
                 </li>
             </ul>
-        </div>
+        </div>  
 
-    </nav>
+
+
+          {{-- +++++++++++++++++++++++lo mio++++++++++++++++++++++++++++++ --}}
+          <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
+            <div class="offcanvas-header">
+              <a href="{{secure_url(route('home'))}}">
+                <img src="{{ secure_asset('tvc.png') }}" class="logo-tvc offcanvas-title" alt="logo tvc"  id="offcanvasNavbarLabel">
+            </a>
+              <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+              <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Dropdown
+                  </a>
+                  <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Action</a></li>
+                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                    <li>
+                      <hr class="dropdown-divider">
+                    </li>
+                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <form class="d-flex mt-3" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </nav>
+
+    {{-- *************************************************** --}}
+
+    
     <main>
         @yield('content')
     </main>
     <footer>
 
     </footer>
+    @yield('scripts')
 </body>
-@yield('scripts')
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
 
