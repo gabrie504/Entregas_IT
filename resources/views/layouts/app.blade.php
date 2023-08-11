83752861b3e3da703dbb6427f8eb64a64de4e937
Gabriel Meza <43562881+gabrie504@users.noreply.github.com>
2023-08-10 22:29:43 -0600

    tratanado de solicionar pwa2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,12 +5,12 @@
 
   
     <meta charset="utf-8">
-    <link rel="manifest" crossorigin="use-credentials" href="{{secure_asset('/manifest.json')}}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
-
+    
+    
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <link rel="manifest" crossorigin="use-credentials" href="{{secure_asset('manifest.json')}}">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
